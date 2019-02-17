336f257dc4d850c230d3a8c8c67ff5dcca8a3472
Karen Kepner <kkepner14 (at) gmail.com>
2019-02-17 13:27:40 -0700

    add nav popup on hamburger click in mobile

--- js/main.js
+++ js/main.js
@@ -1,8 +1,12 @@
 let burger = document.getElementById('hamburger');
 let mainNav = document.getElementById('main-nav');
+let hidden = document.querySelector('.hidden');
 //want to open the navigation on click of the hamburger icon in mobile sizes.
 // doesn't work yet
 burger.addEventListener('click', function () {
   mainNav.style.display = 'flex';
+  hidden.toggleAttribute('class');
   console.log('hamburger clicked')
+
 })
+
