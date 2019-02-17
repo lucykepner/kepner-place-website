39b7f6cf0f8944fb4acdf17bbf7cb5c1948d1961
Karen Kepner <kkepner14 (at) gmail.com>
2019-02-17 16:25:17 -0700

    update resume and improve navigation

--- js/main.js
+++ js/main.js
@@ -1,11 +1,8 @@
 let burgerButton = document.getElementById('hamburger');
-let mainNav = document.getElementById('main-nav');
 let hidden = document.querySelector('.hidden');
-//want to open the navigation on click of the hamburger icon in mobile sizes.
-// doesn't work yet
+
+//open the navigation on click of the hamburger icon in mobile sizes.
 burgerButton.addEventListener('click', function () {
-  // mainNav.style.display = 'flex';
   hidden.toggleAttribute('hidden');
-  
 })
 
